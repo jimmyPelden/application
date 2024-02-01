bf7d75ccbbedeaf1cba1646280798cfa5215d7bf
jimmyPelden <150887932+jimmyPelden@users.noreply.github.com>
2024-02-01 15:40:22 +0600

    Add files via upload

---
+++ my server/index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.Port;
+const hendle = (FileName, statustCode, req, res) => {
+    fs.readFile(FileName, "utf-8",
+    (err, data) => {
+        if (err){
+            console.log(err);
+        }else{
+            res.writeHead (200 , {
+                "content-type" : "text/html"
+            });
+            res.write(data);
+        }
+    }
+    )
+}
+const server = http.createServer((req, res) => {
+   if(req.url === "/"){
+  hendle("index.html", 200, req, res);
+   }else if (req.url === "/about"){
+    hendle("about.html", 200, req, res);
+   }
+   else if (req.url === "/contact"){
+    hendle("contact.html", 200, req, res);
+   }
+   else{
+    hendle("404.html", 200, req, res);
+}
+});
+server.listen(Port, () => {
+    console.log('server is running')
+});
